727244fb318d7325cd4b0030e1b035b523baccfa
Sophie Muchiri <muthonisophie12@gmail.com>
2024-07-09 21:07:38 +0300

    Filter Method

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => 
+      typeof driver === 'string' && driver.toLowerCase() === name.toLowerCase()
+    );
+  }
+  
+  // fuzzyMatch function
+  function fuzzyMatch(drivers, letters) {
+    const lengthOfLetters = letters.length;
+    return drivers.filter(driver => 
+      typeof driver === 'string' && driver.slice(0, lengthOfLetters).toLowerCase() === letters.toLowerCase()
+    );
+  }
+  
+  // matchName function
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  module.exports = { findMatching, fuzzyMatch, matchName };
